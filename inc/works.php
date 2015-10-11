b6c9eb07c7cd1371ccbd9681d939832ffe4b790d
jxie007 <jxie007@e.ntu.edu.sg>
2015-10-11 16:41:49 +0800

    modify the introduction content

--- inc/works.php
+++ inc/works.php
@@ -2,20 +2,20 @@
 
 	$works = array();
 	$works["shirtsShop"] = array(
-		"name" => "An online shirts shop",
+		"name" => "DaTouLi online shirts shop",
 		"intro" => "The website is an online shop selling shirts. The store owner receives payment through PayPal.",
 		"img" => "img/shirtsShop.png",
 		"url" => "http://onlineshop.xiejun.be"
 	);
 	$works["paperlessCheckout"] = array(
-		"name" => "Checkout without paper receipt but with QR code",
-		"intro" => "This web application works with an Android App named Green Checkout. It is mimicing a supermarket's checkout syste. It replace the tradational way to checkout by receiving a paper receipt. Instead, customer receive a digital receipt on his mobile phone. On the website's checkout page, keying in '007' in 'good's code' stands for buying pens, then keying in '5' in the 'quantity' box means you buy 5 pens. Then key in 'aa' stands for your user id in the 'enter user_id here' box. Finally, press the 'checkout' button and the application will show you a QR code. Installed the 'Green checkout' Android App to scan the QR code and enjoy viewing and storing your digial receipt on you mobile phone.",
-		"img" => "img/paperlessCheckout.png",
+		"name" => "Paperless Checkout - checkout without paper receipt but with QR code",
+		"intro" => "This web application works with an Android App named &quot;Green Checkout&quot;. They are usilising QR code to achieve eliminating the usage of paper recipt in the supermarkets.<br><br>The web application mimics a supermarket&#39;s checkout system. It replaces the tradational way to checkout by receiving a paper receipt. Instead, a customer receives a digital receipt on his mobile phone.<br><br>On the website&#39;s checkout page, keying in &#39;007&quot; in &quot;good&#39;s code&quot; stands for buying pens, then keying in &quot5&quot in the &quotquantity&quot box means you buy 5 pens. Then keying in &quotaa&quot stands for providing your user id in the &quotenter user_id here&quot box. Finally, press the &quotcheckout&quot button and the application will show you a QR code.<br><br>Install the &quotGreen checkout&quot Android App to scan the QR code and enjoy viewing and storing your digial receipts on you mobile phone.",
+		"img" => "img/paperlessCheckout.jpg",
 		"url" => "http://paperlessCheckout.xiejun.be/index.php"
 	);
 	$works["greenCheckout"] = array(
-		"name" => "An Android App to replace paper receipt",
-		"intro" => "The website is an online shop selling shirts. The store owner receives payment through PayPal.",
+		"name" => "Green Checkout - an Android App to replace paper receipt",
+		"intro" => "This Android App works with a web application called &quot;Paperless Checkout&quot;. They are usilising QR code to achieve eliminating the usage of paper recipt in the supermarkets.<br><br>Install the App and click the scan button to scan the QR code generated and then view your digial receipts on your mobile phone. Also, you can view your shopping histories by clicking the history button.<br><br>A predefined user id &quot;aa&quot; is available for you to play around. :)",
 		"img" => "img/greenCheckout.jpg",
 		"link_label" => 'Download',
 		"url" => "http://paperlessCheckout.xiejun.be/GreenCheckout.apk"
